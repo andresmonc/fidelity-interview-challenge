2cab3708d62f09172cb8b5a7a66ce361caf84fe8
andresmonc <jaimeandresmonc@gmail.com>
2020-11-03 14:34:03 -0600

    working get/default 404

--- server.go
+++ server.go
@@ -5,21 +5,21 @@ import (
 	"net/http"
 )
 
-
 type Bar struct {
-	uuid   int `json:"uuid"`
-	bar    int `json:"bar"`
+	uuid int `json:"uuid"`
+	bar  int `json:"bar"`
 }
+
 // acting as database
 var Bars = []Bar{
 	Bar{uuid: 123, bar: 13},
 	Bar{uuid: 123, bar: 2},
 }
 
-
 func main() {
 	port := "8080"
 	fmt.Printf("Starting server at port %s", port)
+	http.HandleFunc("/", home)
 	startServer(port)
 }
 
@@ -29,3 +29,25 @@ func startServer(port string) {
 	}
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+        w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+	default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(`{"message": "not found"}`))
+	}
+}
+
+
+func  getBarById(id int) (bar Bar){
+	for i := 0; i < len(Bars); i++ {
+		currentBar := Bars[i]
+		if currentBar.uuid == id {
+			return currentBar
+		}
+	}
+	return
+}
