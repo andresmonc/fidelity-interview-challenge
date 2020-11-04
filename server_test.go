f8a26b13bcc842bba46761c265c3785a59f8eee6
andresmonc <jaimeandresmonc@gmail.com>
2020-11-04 14:58:41 -0600

    added new unit tests

--- server_test.go
+++ server_test.go
@@ -26,6 +26,26 @@ func Test_deleteBarByID(t *testing.T) {
 	assertTrue(deleteBarByID("223456789") == "Success")
 }
 
+func Test_addNewBar(t *testing.T) {
+	barsLen := len(barsData.Bars)
+	bar := Bar{UUID: "223456789", Bar: 1}
+	addNewBar(bar)
+	assertEquals(barsLen+1, len(barsData.Bars))
+}
+
 func Test_getBarByID(t *testing.T) {
 	assertEquals(99, getBarByID("123456789").Bar)
+}
+
+func Test_marshalBar(t *testing.T){
+	marshalBar(barsData.Bars[0])
+}
+
+func Test_marshalBars(t *testing.T){
+	marshalBars(barsData)
+}
+
+
+func Test_unMarshalBars(t *testing.T){
+	// unMarshalBar()
 }
